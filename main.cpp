ec7f9acd630e31c33f8fc937927800f3cfdbe011
Louiss3434 <134706487+Louiss3434@users.noreply.github.com>
2026-02-05 06:12:34 +0800

    fix n clean

--- main.cpp
+++ main.cpp
@@ -7,8 +7,7 @@ typedef short(__stdcall* lpInp32)(short);
 
 #define EC_SC          0x66 
 #define EC_DATA        0x62 
-#define EC_WRITE_CMD   0x81  // Protocol for Power LED
-#define EC_LED_CMD     0x10  // Protocol for F4/Mic LED
+#define EC_WRITE_CMD   0x81
 
 const char* STATE_FILE = "led_state.bin";
 
@@ -18,7 +17,6 @@ bool WaitEC(lpInp32 gInp32) {
     return timeout > 0;
 }
 
-// Simple wrapper for the 0x81 (Write Register) protocol
 void WriteReg(lpOut32 gOut32, lpInp32 gInp32, BYTE addr, BYTE val) {
     if (!WaitEC(gInp32)) return;
     gOut32(EC_SC, EC_WRITE_CMD);
@@ -34,31 +32,20 @@ int main() {
     lpOut32 gOut32 = (lpOut32)GetProcAddress(hInpOutDll, "Out32");
     lpInp32 gInp32 = (lpInp32)GetProcAddress(hInpOutDll, "Inp32");
 
-    // 1. FORCE MIC (F4) OFF EVERY TIME
-    // 0x02 is the ID for Mic LED, 0x00 is the "Off" mode
-    if (WaitEC(gInp32)) {
-        gOut32(EC_SC, EC_LED_CMD); 
-        if (WaitEC(gInp32)) gOut32(EC_DATA, 0x02); 
-    }
-
-    // 2. TOGGLE POWER LED
     bool isOff = false;
     std::ifstream inFile(STATE_FILE);
     if (inFile.is_open()) { inFile >> isOff; inFile.close(); }
 
     if (isOff) {
-        // TURN ON: Just set the master switch
         WriteReg(gOut32, gInp32, 0x0C, 0x80); 
-        WriteReg(gOut32, gInp32, 0xA0, 0x80); // Brightness up
+        WriteReg(gOut32, gInp32, 0xA0, 0x80); 
         isOff = false;
     } else {
-        // TURN OFF: Kill the switch and the brightness
         WriteReg(gOut32, gInp32, 0x0C, 0x00); 
-        WriteReg(gOut32, gInp32, 0xA0, 0x00); // Set brightness 0
+        WriteReg(gOut32, gInp32, 0xA0, 0x00);
         isOff = true;
     }
 
-    // 3. SAVE STATE
     std::ofstream outFile(STATE_FILE);
     if (outFile.is_open()) { outFile << isOff; outFile.close(); }
 
